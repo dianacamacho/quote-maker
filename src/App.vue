cd4b86a4c789ac99fd73a570bbde43afb2ed0eb2
dianacamacho <dcam1901@yahoo.com>
2017-01-31 16:09:36 -0600

    components for quote, quote grid, new quote, method to add new quote

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+<!-- when listening for emitted event, vue auto passes the $event (the new quote in this case), so don't really need to write in the argument like below, but you can -->
+    <!-- <appNewQuote @quoteAdded="newQuote($event)"></appNewQuote> -->
+    <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+    <appQuoteGrid :quotes="quotes"></appQuoteGrid>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue'
+  import NewQuote from './components/NewQuote.vue'
+ 
+  export default {
+    data: function() {
+      return {
+        quotes: ['Just a quote'],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <appQuote v-for="quote in quotes">
+      <p>{{ quote }}</p>
+    </appQuote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+  
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+  
+</style>
